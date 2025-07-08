f52345514537f6b71f1318210eed32da8e31a4f3
Himawari Lad <Sacrificial.animal@gmail.com>
2025-07-08 03:35:58 +0300

    Code added 4

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        {
             ////////////////////////////
             System.out.println("CASE 1");
             int[] money = {14000, 24000, 34000, 40000, 20000};
             int sum = 0;
-            for (int i = 0; i <= 4; i++) {
-                sum = sum + money[i];
-            }
-            System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        for (int i : money) {
+            sum += i;
         }
+            System.out.println("Сумма трат за месяц составила " + sum + " рублей");
 
-        {
+
+
             ////////////////////////////
             System.out.println("CASE 2");
-            int[] money = {14000, 24000, 34000, 40000, 20000};
             int minMoney = money[0];
             int maxMoney = money[0];
 
@@ -31,24 +29,22 @@ public class Main {
                 System.out.println("Минимальная сумма трат за неделю составила " +
                         minMoney + " рублей. Максимальная сумма трат за неделю составила " +
                         maxMoney + " рублей.");
-            }
 
-        {
+
+
+
             ////////////////////////////
             System.out.println("CASE 3");
-            int[] money = {14000, 24000, 34000, 40000, 20000};
-            int sum = 0;
-            int i = 0;
-            for (i = 0; i <= 4; i++) {
-                sum = sum + money[i];
-            }
-                double average = sum / i;
+            sum = 0;
+        for (int j : money) {
+            sum = sum + j;
+        }
+                double average = (double) sum / money.length;
 
                 System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
 
-        }
 
-        {
+
             ////////////////////////////
             System.out.println("CASE 4");
             char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
@@ -65,8 +61,5 @@ public class Main {
             for (char c : reverseFullName) {
                 System.out.print(c);
             }
-
-        }
-
         }
     }
